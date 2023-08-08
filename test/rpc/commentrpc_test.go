e1e8e6f1509f959b249068c7770728407d81c7ee
yangfeng <1719957182@qq.com>
2023-08-09 00:21:59 +0800

    fix(comment): fix commentrpc_test

--- test/rpc/commentrpc_test.go
+++ test/rpc/commentrpc_test.go
@@ -2,7 +2,6 @@ package rpc
 
 import (
 	"GuGoTik/src/constant/config"
-	"GuGoTik/src/constant/strings"
 	"GuGoTik/src/rpc/comment"
 	"context"
 	"fmt"
@@ -27,7 +26,7 @@ func TestActionComment_Add(t *testing.T) {
 		Action:     &comment.ActionCommentRequest_CommentText{CommentText: "Test comment"},
 	})
 	assert.Empty(t, err)
-	assert.Equal(t, strings.ServiceOKCode, res.StatusCode)
+	assert.Equal(t, uint32(0), res.StatusCode)
 }
 
 func TestActionComment_Delete(t *testing.T) {
