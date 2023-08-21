81d479801abd5969fb10495b9d76b0c6ec7a8ab5
文杰 <2707138687@qq.com>
2023-08-21 19:38:15 +0800

    refactor: resolve conflicts

--- src/models/rawvideo.go
+++ src/models/rawvideo.go
@@ -3,11 +3,11 @@ package models
 import "GuGoTik/src/storage/database"
 
 type RawVideo struct {
-	ActorId   uint32
-	VideoId   uint32
-	Title     string
-	FileName  string
-	CoverName string
+	ActorId   uint32 `gorm:"not null;primarykey;"`
+	VideoId   uint32 `json:"video_id" column:"video_id" gorm:"not null;"` // 视频 ID
+	Title     string `json:"title" gorm:"not null;"`
+	FileName  string `json:"play_name" gorm:"not null;"`
+	CoverName string `json:"cover_name" gorm:"not null;"`
 }
 
 func init() {
