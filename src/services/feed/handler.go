38d79539b9cfdf0241c4f9a23bbac98377507ccc
yangfeng <1719957182@qq.com>
2023-08-21 23:03:26 +0800

    feat(video): add user id info to video link

--- src/services/feed/handler.go
+++ src/services/feed/handler.go
@@ -202,7 +202,7 @@ func queryDetailed(
 		// fill play url
 		go func(i int, v *models.Video) {
 			defer wg.Done()
-			playUrl, localErr := file.GetLink(ctx, v.FileName)
+			playUrl, localErr := file.GetLink(ctx, v.FileName, v.UserId)
 			if localErr != nil {
 				logger.WithFields(logrus.Fields{
 					"video_id":  v.ID,
@@ -218,7 +218,7 @@ func queryDetailed(
 		// fill cover url
 		go func(i int, v *models.Video) {
 			defer wg.Done()
-			coverUrl, localErr := file.GetLink(ctx, v.CoverName)
+			coverUrl, localErr := file.GetLink(ctx, v.CoverName, v.UserId)
 			if localErr != nil {
 				logger.WithFields(logrus.Fields{
 					"video_id":   v.ID,
