c6dbe9719f9556c81a97b8178dde5763785ddd04
EpicMo <1982742309@qq.com>
2023-08-21 23:15:36 +0800

    feat: add ut

--- test/rpc/publishrpc_test.go
+++ test/rpc/publishrpc_test.go
@@ -8,6 +8,8 @@ import (
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"io"
+	"os"
 	"testing"
 )
 
@@ -39,6 +41,27 @@ func TestCountVideo(t *testing.T) {
 	assert.Empty(t, err)
 	Client = publish.NewPublishServiceClient(conn)
 	res, err := Client.CountVideo(context.Background(), &req)
+	assert.Empty(t, err)
+	assert.Equal(t, int32(0), res.StatusCode)
+}
+
+func TestPublishVideo(t *testing.T) {
+	var Client publish.PublishServiceClient
+	reader, err := os.Open("F:\\GuGo\\mysea1.mp4")
+	assert.Empty(t, err)
+	bytes, err := io.ReadAll(reader)
+	assert.Empty(t, err)
+	req := publish.CreateVideoRequest{
+		ActorId: 2,
+		Data:    bytes,
+		Title:   "原神，启动！",
+	}
+	conn, err := grpc.Dial(fmt.Sprintf("127.0.0.1%s", config.PublishRpcServerPort),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy": "round_robin"}`))
+	assert.Empty(t, err)
+	Client = publish.NewPublishServiceClient(conn)
+	res, err := Client.CreateVideo(context.Background(), &req)
 	assert.Empty(t, err)
 	assert.Equal(t, int32(0), res.StatusCode)
 }
