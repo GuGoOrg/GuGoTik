f6480155522bb1732685674072f8871926668d6e
EpicMo <1982742309@qq.com>
2023-08-23 11:42:04 +0800

    fix: dockerfile

--- src/services/videoprocessor/main.go
+++ src/services/videoprocessor/main.go
@@ -263,7 +263,7 @@ func textWatermark(ctx context.Context, video *models.RawVideo) (string, error) 
 	defer span.End()
 	logger := logging.LogService("VideoPicker.Picker").WithContext(ctx)
 	// 加载字体文件
-	fontName := file.GetLocalPath(ctx, filepath.Join("..", "..", "static", "font.ttf"))
+	fontName := filepath.Join("static", "font.ttf")
 	fontBytes, err := os.ReadFile(fontName)
 	if err != nil {
 		logger.WithFields(logrus.Fields{
